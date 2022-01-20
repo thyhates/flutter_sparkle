ab0809da3c88abb7c03f5b230be12cd20c658ce7
thyhates <liaozhipu@sina.com>
2022-01-20 11:17:01 +0800

    add windows platform support

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # flutter_sparkle
 A flutter plugin to check update and install your macOS app which use [Sparkle](https://sparkle-project.org/).
 
-Only support for macOS platform.
+Only support for macOS and Windows platform.
 # Setup
-
+## mac
 - Enable network connections for your app
 - Run `./bin/generate_keys` tool (from the Sparkle distribution root) to generate public key.
 - Add your public key to your app’s `Info.plist` as a `SUPublicEDKey` property
---
+++ windows/flutter_sparkle_plugin.cpp
@@ -0,0 +1,82 @@
+#include "include/flutter_sparkle/flutter_sparkle_plugin.h"
+
+// This must be included before many other Windows headers.
+#include <windows.h>
+
+// For getPlatformVersion; remove unless needed for your plugin implementation.
+#include <VersionHelpers.h>
+
+#include <flutter/method_channel.h>
+#include <flutter/plugin_registrar_windows.h>
+#include <flutter/standard_method_codec.h>
+
+#include <map>
+#include <memory>
+#include <sstream>
+
+namespace {
+
+class FlutterSparklePlugin : public flutter::Plugin {
+ public:
+  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+
+  FlutterSparklePlugin();
+
+  virtual ~FlutterSparklePlugin();
+
+ private:
+  // Called when a method is called on this plugin's channel from Dart.
+  void HandleMethodCall(
+      const flutter::MethodCall<flutter::EncodableValue> &method_call,
+      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+};
+
+// static
+void FlutterSparklePlugin::RegisterWithRegistrar(
+    flutter::PluginRegistrarWindows *registrar) {
+  auto channel =
+      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+          registrar->messenger(), "flutter_sparkle",
+          &flutter::StandardMethodCodec::GetInstance());
+
+  auto plugin = std::make_unique<FlutterSparklePlugin>();
+
+  channel->SetMethodCallHandler(
+      [plugin_pointer = plugin.get()](const auto &call, auto result) {
+        plugin_pointer->HandleMethodCall(call, std::move(result));
+      });
+
+  registrar->AddPlugin(std::move(plugin));
+}
+
+FlutterSparklePlugin::FlutterSparklePlugin() {}
+
+FlutterSparklePlugin::~FlutterSparklePlugin() {}
+
+void FlutterSparklePlugin::HandleMethodCall(
+    const flutter::MethodCall<flutter::EncodableValue> &method_call,
+    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
+  if (method_call.method_name().compare("getPlatformVersion") == 0) {
+    std::ostringstream version_stream;
+    version_stream << "Windows ";
+    if (IsWindows10OrGreater()) {
+      version_stream << "10+";
+    } else if (IsWindows8OrGreater()) {
+      version_stream << "8";
+    } else if (IsWindows7OrGreater()) {
+      version_stream << "7";
+    }
+    result->Success(flutter::EncodableValue(version_stream.str()));
+  } else {
+    result->NotImplemented();
+  }
+}
+
+}  // namespace
+
+void FlutterSparklePluginRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar) {
+  FlutterSparklePlugin::RegisterWithRegistrar(
+      flutter::PluginRegistrarManager::GetInstance()
+          ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
+}
